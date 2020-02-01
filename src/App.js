57869d82f6a3f4ba6108d1e59e2d7b9f67da2e34
Rudigus <rodrigo.matos9@hotmail.com>
2020-02-01 18:19:27 -0300

    adicionando login inicial usando google

--- src/App.js
+++ src/App.js
@@ -3,8 +3,76 @@ import React from 'react';
 import './App.css';
 import './global.css'
 import './Main.css'
-
+import firebase from "firebase"
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
 import StudentForm from './components/StudentForm'
+
+// Configure Firebase.
+const config = {
+  apiKey: 'AIzaSyBCDqncv8ZHjdxm2BOTFkDmw-LlKLhfRqY',
+  authDomain: 'ubireudo.firebaseapp.com',
+  // ...
+};
+firebase.initializeApp(config);
+
+class SignInScreen extends React.Component {
+
+  // The component's Local state.
+  state = {
+    isSignedIn: false // Local signed-in state.
+  };
+
+  // Configure FirebaseUI.
+  uiConfig = {
+    // Popup signin flow rather than redirect flow.
+    signInFlow: 'popup',
+    // We will display Google as auth provider.
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      // Avoid redirects after sign-in.
+      signInSuccessWithAuthResult: () => false
+    }
+  };
+
+  // Listen to the Firebase Auth state and set the local state.
+  componentDidMount() {
+    this.unregisterAuthObserver = firebase.auth().onAuthStateChanged(
+        (user) => this.setState({isSignedIn: !!user})
+    );
+  }
+  
+  // Make sure we un-register Firebase observers when the component unmounts.
+  componentWillUnmount() {
+    this.unregisterAuthObserver();
+  }
+
+  render() {
+    if (!this.state.isSignedIn) {
+      return (
+        <div>
+          <h1>My App</h1>
+          <p>Please sign-in:</p>
+          <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={firebase.auth()}/>
+        </div>
+      );
+    }
+    return (
+      <div>
+        <h1>My App</h1>
+        <p>Welcome {firebase.auth().currentUser.displayName}! You are now signed-in!</p>
+        <a onClick={() => firebase.auth().signOut()}>Sign-out</a>
+      </div>
+    );
+  }
+}
+
+function App() {
+  return <SignInScreen></SignInScreen>
+}
+
+/*
 function App() {
 
   return (
@@ -22,5 +90,6 @@ function App() {
 
   );
 }
+*/
 
 export default App;
