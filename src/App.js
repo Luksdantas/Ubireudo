fd318095f140b9de67d404d178637d7252729cf2
Rudigus <rodrigo.matos9@hotmail.com>
2020-02-01 23:54:01 -0300

    troca de fotos pronta

--- src/App.js
+++ src/App.js
@@ -18,6 +18,42 @@ firebase.initializeApp(config);
 var storage = firebase.storage();
 var storageRef = storage.ref();
 
+class FileInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.fileInput = React.createRef();
+    this.state = {image: firebase.auth().currentUser.photoURL};
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    alert(
+      `Selected file - ${
+        this.fileInput.current.files[0].name
+      }`
+    );
+    this.setState({
+      image: URL.createObjectURL(this.fileInput.current.files[0])
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <img alt="Imagem de Perfil" src={this.state.image}></img>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Selecionar Foto:
+            <input type="file" ref={this.fileInput} />
+          </label>
+          <br />
+          <button type="submit">Salvar</button>
+        </form>
+      </div>
+    );
+  }
+}
+
 class SignInScreen extends React.Component {
 
   // The component's Local state.
@@ -64,21 +100,12 @@ class SignInScreen extends React.Component {
     return (
       <div>
         <h1>Ubireudo</h1>
-        <img alt="Imagem de Perfil" src={firebase.auth().currentUser.photoURL}></img>
+        <FileInput></FileInput>
         <p>Seja bem-vindo, {firebase.auth().currentUser.displayName}! Agora você está conectado!</p>
         <a onClick={() => firebase.auth().signOut()}>Desconectar</a>
-        <input type="file" id="avatar" name="avatar"
-       accept="image/png, image/jpeg" onChange={() => handleFiles(this.files)}/>
       </div>
     );
   }
-}
-
-function handleFiles(files) {
-  var avatar = files[0];
-  console.log(avatar.name);
-  // Upload file and metadata to the object 'images/mountains.jpg'
-//var uploadTask = storageRef.child('images/' + file.name).put(file, metadata);
 }
 
 function App() {
