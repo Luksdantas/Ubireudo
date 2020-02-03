4361e4b1894d1d4604b5f1bb046ec9cad276021d
Lucas Dantas <lucasdantasmoura@hotmail.com>
2020-02-03 10:55:45 -0300

    Modificações no CSS

--- src/SignInScreen.js
+++ src/SignInScreen.js
@@ -3,17 +3,17 @@ import FileInput from './FileInput.js'
 import firebase from "firebase"
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
 import TeamManager from './TeamManager.js';
-import {storage, database, storageRef, httpGetAsync, httpPostAsync, httpPutAsync} from './firebase.js'
+import { storage, database, storageRef, httpGetAsync, httpPostAsync, httpPutAsync } from './firebase.js'
 
 var globalUser = null;
 
-firebase.auth().onAuthStateChanged(function(user) {
+firebase.auth().onAuthStateChanged(function (user) {
   if (user) {
     globalUser = user;
     // User is signed in.
     // If creation time is equal to last login time, then the user must be new
-    if (firebase.auth().currentUser.metadata.creationTime === 
-    firebase.auth().currentUser.metadata.lastSignInTime){
+    if (firebase.auth().currentUser.metadata.creationTime ===
+      firebase.auth().currentUser.metadata.lastSignInTime) {
       var userRelevantData = {
         "displayName": user.displayName,
         "photoURL": user.photoURL,
@@ -24,10 +24,10 @@ firebase.auth().onAuthStateChanged(function(user) {
         "lastSignInTime": user.metadata.lastSignInTime,
         "teamCode": 0,
       };
-      httpPostAsync('https://ubireudo.firebaseio.com/users_public/' + user.uid + '.json?auth=' + user.uid, 
-      JSON.stringify(userRelevantData), function(texto) {
-        console.log(texto);
-      });
+      httpPostAsync('https://ubireudo.firebaseio.com/users_public/' + user.uid + '.json?auth=' + user.uid,
+        JSON.stringify(userRelevantData), function (texto) {
+          console.log(texto);
+        });
     }
   } else {
     // No user is signed in.
@@ -36,92 +36,113 @@ firebase.auth().onAuthStateChanged(function(user) {
 
 class SignInScreen extends React.Component {
 
-    // The component's Local state.
-    state = {
-      isSignedIn: false // Local signed-in state.
-    };
-  
-    // Configure FirebaseUI.
-    uiConfig = {
-      // Popup signin flow rather than redirect flow.
-      signInFlow: 'popup',
-      // We will display Google as auth provider.
-      signInOptions: [
-        firebase.auth.GoogleAuthProvider.PROVIDER_ID
-      ],
-      callbacks: {
-        // Avoid redirects after sign-in.
-        signInSuccessWithAuthResult: () => false
-      }
-    };
-  
-    // Listen to the Firebase Auth state and set the local state.
-    componentDidMount() {
-      this.unregisterAuthObserver = firebase.auth().onAuthStateChanged(
-        (user) => this.setState({ isSignedIn: !!user })
-      );
+  // The component's Local state.
+  state = {
+    isSignedIn: false // Local signed-in state.
+  };
+
+  // Configure FirebaseUI.
+  uiConfig = {
+    // Popup signin flow rather than redirect flow.
+    signInFlow: 'popup',
+    // We will display Google as auth provider.
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      // Avoid redirects after sign-in.
+      signInSuccessWithAuthResult: () => false
     }
-  
-    // Make sure we un-register Firebase observers when the component unmounts.
-    componentWillUnmount() {
-      this.unregisterAuthObserver();
-    }
-  
-    render() {
-      if (!this.state.isSignedIn) {
-  
-        return (
-          <div id="login">
-            <main>
-              <strong>Ubireudo</strong>
-              <div className="input-block">
-                <label htmlFor="nome_aluno">Faça o cadastro usando uma das opções abaixo:</label>
-              </div>
-              <div className="input-block">
-                <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={firebase.auth()} />
-              </div>
-            </main>
-          </div >
-        );
-  
-      }
-      return (
-  
-        <div id="app">
-          <aside>
-            <strong>Ubireudo</strong>
-            <div className="input-block">
-              <label>Seja bem-vindo, {firebase.auth().currentUser.displayName}!</label>
-            </div>
-            <div className="input-block">
-              <FileInput></FileInput>
-            </div>
-            <button onClick={() => firebase.auth().signOut()}>Desconectar</button>
-          </aside>
-  
-          <main>
-            <ul>
-  
-              <li className="turmas">
-                <header>
-                  <img src="" alt="img" />
-                  <div className="informacoes">
-                    <strong>nome da turma</strong>
-                    <span>descrição</span>
-                  </div>
-                </header>
-                <p>pontuação</p>
-                <TeamManager></TeamManager>
-              </li>
-  
-              
-            </ul>
-          </main>
-        </div>
-  
-      );
-    }
+  };
+
+  // Listen to the Firebase Auth state and set the local state.
+  componentDidMount() {
+    this.unregisterAuthObserver = firebase.auth().onAuthStateChanged(
+      (user) => this.setState({ isSignedIn: !!user })
+    );
   }
 
-  export default SignInScreen;
-  export {globalUser};
+  // Make sure we un-register Firebase observers when the component unmounts.
+  componentWillUnmount() {
+    this.unregisterAuthObserver();
+  }
+
+  render() {
+    if (!this.state.isSignedIn) {
+
+      return (
+        <div id="login">
+          <main>
+            <strong>Ubireudo</strong>
+            <div className="input-block">
+              <label htmlFor="nome_aluno">Faça o cadastro usando uma das opções abaixo:</label>
+            </div>
+            <div className="input-block">
+              <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={firebase.auth()} />
+            </div>
+          </main>
+        </div >
+      );
+
+    }
+    return (
+
+      <div id="app">
+        <aside>
+          <strong>Ubireudo</strong>
+          <div className="input-block">
+            <label>Seja bem-vindo, {firebase.auth().currentUser.displayName}!</label>
+          </div>
+          <div className="input-block">
+            <FileInput></FileInput>
+          </div>
+          <TeamManager></TeamManager>
+          <button onClick={() => firebase.auth().signOut()}>Desconectar</button>
+        </aside>
+
+        <main>
+          <ul>
+
+            <li className="turmas">
+              <header>
+                <img src="" alt="img" />
+                <div className="informacoes">
+                  <strong>nome da turma</strong>
+                  <span>professor</span>
+                </div>
+              </header>
+              <p>pontuação</p>
+            </li>
+
+            <li className="turmas">
+              <header>
+                <img src="" alt="img" />
+                <div className="informacoes">
+                  <strong>nome da turma</strong>
+                  <span>professor</span>
+                </div>
+              </header>
+              <p>pontuação</p>
+            </li>
+
+            <li className="turmas">
+              <header>
+                <img src="" alt="img" />
+                <div className="informacoes">
+                  <strong>nome da turma</strong>
+                  <span>professor</span>
+                </div>
+              </header>
+              <p>pontuação</p>
+            </li>
+
+          </ul>
+        </main>
+      </div>
+
+    );
+  }
+}
+
+export default SignInScreen;
+export { globalUser };
--- src/TeamManager.js
+++ src/TeamManager.js
@@ -1,51 +1,56 @@
 import React, { useState, useEffect } from 'react';
 import { httpGetAsync, httpPutAsync } from './firebase';
-import { globalUser} from './SignInScreen.js';
+import { globalUser } from './SignInScreen.js';
 
 class TeamManager extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        isManagerOpen: false,
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isManagerOpen: false,
     }
+  }
 
-    handleSubmit(event) {
-      event.preventDefault();
-      httpGetAsync('https://ubireudo.firebaseio.com/users_public/' + globalUser.uid + 
-      '.json?auth=' + globalUser.uid, function(texto) {
+  handleSubmit(event) {
+    event.preventDefault();
+    httpGetAsync('https://ubireudo.firebaseio.com/users_public/' + globalUser.uid +
+      '.json?auth=' + globalUser.uid, function (texto) {
         var userData = JSON.parse(texto);
         userData.teamCode = event.target.teamCode.value;
-        httpPutAsync('https://ubireudo.firebaseio.com/users_public/' + globalUser.uid + 
-        '.json?auth=' + globalUser.uid, JSON.stringify(userData), function(texto) {
-        console.log(texto);
+        httpPutAsync('https://ubireudo.firebaseio.com/users_public/' + globalUser.uid +
+          '.json?auth=' + globalUser.uid, JSON.stringify(userData), function (texto) {
+            console.log(texto);
+          });
       });
-      });
-    }
-  
-    render() {
-      if(!this.state.isManagerOpen)
-      {
-        return (
-          <div>
-            <button onClick={() => this.setState(prevState => ({
-            isManagerOpen: !prevState.isManagerOpen}))}>Gerenciador de Turmas</button>
-          </div>
-          );
-      }
+  }
 
+  render() {
+    if (!this.state.isManagerOpen) {
       return (
         <div>
           <button onClick={() => this.setState(prevState => ({
-            isManagerOpen: !prevState.isManagerOpen}))}>Gerenciador de Turmas</button>
-          <form onSubmit={this.handleSubmit}>
-            <label>Código da turma:</label>
-            <input type="text" name="teamCode"></input>
-            <button type="submit">Salvar</button>
-          </form>
-        </div>)
-    
+            isManagerOpen: !prevState.isManagerOpen
+          }))}>Gerenciador de Turmas</button>
+        </div>
+      );
     }
+
+    return (
+      <div>
+        <button onClick={() => this.setState(prevState => ({
+          isManagerOpen: !prevState.isManagerOpen
+        }))}>Gerenciador de Turmas</button>
+        <form onSubmit={this.handleSubmit}>
+          <label>Código da turma:</label>
+          <div id="entrarturma">
+
+            <input type="text" name="teamCode"></input>
+            <button type="submit">Entrar na Turma</button>
+
+          </div>
+        </form>
+      </div>)
+
+  }
 }
 
-  export default TeamManager;
+export default TeamManager;
