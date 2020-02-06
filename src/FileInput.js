ce42218fe9fd7f9024f4665beb7d8a038c3033a9
Rudigus <rodrigo.matos9@hotmail.com>
2020-02-05 22:18:48 -0300

    imagem puxada do database

--- src/FileInput.js
+++ src/FileInput.js
@@ -1,19 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import firebase from "firebase"
-import Cropper from 'react-easy-crop'
 import { storageRef} from './firebase.js'
-
-var urlImage = null;
 
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
-    firebase.database().ref('users_public/' + user.uid + "/urlImage").on('value', function(snapshot) {
-      urlImage = snapshot.val();
-    });
-    alert(
-      `Arquivo escolhido - ${this.fileInput.current.files[0].name
-      }`
-    );
+    console.log("User logged in.");
   } else {
     // No user is signed in.
   }
@@ -26,60 +17,41 @@ class FileInput extends React.Component {
     this.fileInput = React.createRef();
     this.state = {
       image: firebase.auth().currentUser.photoURL,
-      crop: { x: 0, y: 0 },
-      zoom: 1,
-      aspect: 4 / 3,
-    };
+    }
   }
 
-  onCropChange = crop => {
-    this.setState({ crop })
-  }
-
-  onCropComplete = (croppedArea, croppedAreaPixels) => {
-    console.log(croppedArea, croppedAreaPixels)
-  }
-
-  onZoomChange = zoom => {
-    this.setState({ zoom })
+  componentDidMount() {
+    firebase.database().ref('users_public/' + firebase.auth().currentUser.uid + "/urlImage").on('value', snapshot => {
+      console.log("ok");
+      this.setState({image: snapshot.val()})
+     // document.getElementById("profilePhoto").src = snapshot.val();
+    });
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    // alert(
-    //   `Arquivo escolhido - ${
-    //   this.fileInput.current.files[0].name
-    //   }`
-    // );
-    this.setState({
-      image: URL.createObjectURL(this.fileInput.current.files[0])
-    });
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.image != this.state.image) {
-      var firebasePhoto = this.fileInput.current.files[0];
-      var uploadTask = storageRef.child('images/' + firebasePhoto.name).put(firebasePhoto);
-      // Register three observers:
-      // 1. 'state_changed' observer, called any time the state changes
-      // 2. Error observer, called on failure
-      // 3. Completion observer, called on successful completion
-      uploadTask.on('state_changed', function (snapshot) {
-        // Observe state change events such as progress, pause, and resume
-        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log('Upload is ' + progress + '% done');
-        switch (snapshot.state) {
-          case firebase.storage.TaskState.PAUSED: // or 'paused'
-            console.log('Upload is paused');
-            break;
-          case firebase.storage.TaskState.RUNNING: // or 'running'
-            console.log('Upload is running');
-            break;
-        }
-      }, function (error) {
-        // Handle unsuccessful uploads
-      }, function () {
+    var firebasePhoto = this.fileInput.current.files[0];
+    var uploadTask = storageRef.child('images/' + firebasePhoto.name).put(firebasePhoto);
+    // Register three observers:
+    // 1. 'state_changed' observer, called any time the state changes
+    // 2. Error observer, called on failure
+    // 3. Completion observer, called on successful completion
+    uploadTask.on('state_changed', function (snapshot) {
+      // Observe state change events such as progress, pause, and resume
+      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log('Upload is ' + progress + '% done');
+      switch (snapshot.state) {
+        case firebase.storage.TaskState.PAUSED: // or 'paused'
+          console.log('Upload is paused');
+          break;
+        case firebase.storage.TaskState.RUNNING: // or 'running'
+          console.log('Upload is running');
+          break;
+      }
+    }, function (error) {
+      // Handle unsuccessful uploads
+    }, function () {
         // Handle successful uploads on complete
         // For instance, get the download URL: https://firebasestorage.googleapis.com/...
         uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL) {
@@ -89,24 +61,13 @@ class FileInput extends React.Component {
           });
         });
       });
-
-      // Sistema de persistência de foto de perfil
-      //var user = firebase.auth().currentUser;
-      //user.updateProfile({
-      //  photoURL: 
-      //}).then(function() {
-      // Update successful.
-      //}).catch(function(error) {
-      // An error happened.
-      //});
-    }
   }
 
   render() {
     return (
       <div>
         <div className="input-block">
-          <img alt="Imagem de Perfil" src={this.state.image}></img>
+          <img id="profilePhoto" alt="Imagem de Perfil" src={this.state.image}></img>
         </div>
         <div className="input-block">
           <form onSubmit={this.handleSubmit}>
@@ -121,17 +82,5 @@ class FileInput extends React.Component {
     );
   }
 }
-
-/*<div>
-  <Cropper
-    image={this.state.image}
-    crop={this.state.crop}
-    zoom={this.state.zoom}
-    aspect={this.state.aspect}
-    onCropChange={this.onCropChange}
-    onCropComplete={this.onCropComplete}
-    onZoomChange={this.onZoomChange}
-  />
-</div> */
 
 export default FileInput;
